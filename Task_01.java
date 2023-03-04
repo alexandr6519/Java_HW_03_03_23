96b9c6e6b8d0d37556dd85adb35366c98fc925d8
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2023-03-04 16:17:20 +0700

    adding of task_02

--- Task_01.java
+++ Task_01.java
@@ -15,27 +15,28 @@ class Task_01 {
         return sum;
     }
 
-    static int get_factorial (int n) {
+    static int get_factorial(int n) {
         int result = 1;
         for (int index = 0; index < n; index++) {
             result *= index + 1;
         }
         return result;
     }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = 0;
         System.out.printf("Enter natural number: ");
-        if (!scn.hasNextInt()){
-            System.out.println("You entered wrong number!!!");   
+        if (!scn.hasNextInt()) {
+            System.out.println("You entered wrong number!!!");
         } else {
             n = scn.nextInt();
             if (n < 1)
-                System.out.println("You should enter positive number only!!!");   
-            else {                     
+                System.out.println("You should enter positive number only!!!");
+            else {
                 System.out.printf("The triangle number of %d is: %s! ", n, Task_01.get_triangle_number(n));
                 System.out.println();
-                System.out.printf("The factorial of %d is: %s! ", n, Task_01.get_factorial(n));        
+                System.out.printf("The factorial of %d is: %s! ", n, Task_01.get_factorial(n));
             }
         }
         scn.close();
---
+++ Task_02.java
@@ -0,0 +1,24 @@
+//Вывести все простые числа от 1 до 1000
+package HW_03_03_23;
+
+public class Task_02 {
+    boolean is_simple(int n) {
+        for (int index = 2; index < n; index++) {
+            if (n % index == 0 )
+                return false;
+        }
+        return true;
+
+    }
+    public static void main(String[] args) {
+        Task_02 t = new Task_02();
+        StringBuilder st = new StringBuilder();
+        st = st.append("1");
+        for (int i = 2; i < 1000; i++) {
+            if (t.is_simple(i))
+                st = st.append(", ").append(i);
+        }
+    System.out.println("This is order of simple numbers from 1 to 1000:");   
+    System.out.println(st);   
+    }
+}
