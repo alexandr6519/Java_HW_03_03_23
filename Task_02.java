c0dcddeb180c4fe66cd3ed00e4fb0efffd3f2bfd
alexandr6519 <46781356+alexandr6519@users.noreply.github.com>
2023-03-09 10:46:26 +0700

    Update Task_02.java

--- Task_02.java
+++ Task_02.java
@@ -1,8 +1,8 @@
-//Вывести все простые числа от 1 до 1000
+i//Вывести все простые числа от 1 до 1000
 package Java_HW_03_03_23;
 
 public class Task_02 {
-    boolean is_simple(int n) {
+    boolean isNumberSimple(int n) {
         for (int index = 2; index < n; index++) {
             if (n % index == 0 )
                 return false;
@@ -11,11 +11,10 @@ public class Task_02 {
 
     }
     public static void main(String[] args) {
-        Task_02 t = new Task_02();
         StringBuilder st = new StringBuilder();
         st = st.append("1");
         for (int i = 2; i < 1000; i++) {
-            if (t.is_simple(i))
+            if (isNumberSimple(i))
                 st = st.append(", ").append(i);
         }
     System.out.println("This is order of simple numbers from 1 to 1000:");   
